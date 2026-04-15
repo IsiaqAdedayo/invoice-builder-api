eb51169424bc9d9b51045c512e2c2e26c8756168
isiaqAdedayo <hadedaryor@gmail.com>
2026-04-15 21:44:56 +0100

    ssl required for neon

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('register')
+  @Post('signup')
   register(@Body() dto) {
     return this.authService.register(dto);
   }
