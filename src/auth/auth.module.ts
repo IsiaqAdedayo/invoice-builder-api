7711ef313448c40b31b9858ff6508a9fbc0e36a9
isiaqAdedayo <hadedaryor@gmail.com>
2026-04-15 21:15:11 +0100

    ssl required for neon

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { User } from 'src/users/entities/user.entity';
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: 'secret_key',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
   ],
